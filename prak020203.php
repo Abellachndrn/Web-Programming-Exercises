ecde20329ef181691f0101345dc269fd11275568
Abella Chandra <71272956+Abellachndrn@users.noreply.github.com>
2021-03-25 17:52:35 +0700

    Update prak020203.php

--- prak020203.php
+++ prak020203.php
@@ -7,7 +7,7 @@ for($i = 0; $i < $baris ; $i++){
 	echo ("<tr>");
  	for ($j = 0; $j < $kolom ; $j++){
 			if ($angka % 2 == 0){
-				echo ("<td style=color:black;background-color:red;>${angka}</td>");
+				echo ("<td style=color:white;background-color:red;>${angka}</td>");
 			}else {
 				echo ("<td style=color:red;background-color:white;>${angka}</td>");
 			}
